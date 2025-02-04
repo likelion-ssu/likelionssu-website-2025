90e7b134c7181254906ec85af6f901165498851f
hyun907 <seunghyun020907@naver.com>
2025-02-05 02:24:07 +0900

    feat: 메인 페이지 레이아웃

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,5 +1,158 @@
+import styled from "styled-components";
+import bgImg from "./assets/background.png";
+import Arrow_IC from "./assets/ic_arrow_up_right.svg?react";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  return <div>main 페이지입니다.</div>;
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate(`/about`);
+  };
+  
+  return (
+    <>
+      <BgImgWrapper />
+      <BgWrapper />
+      <MainContainer>
+        <TextContainer>
+          <Title>Possibility To Reality,</Title>
+          <SubTitle>숭실대학교 멋쟁이사자처럼에서 실현하세요!</SubTitle>
+        </TextContainer>
+      </MainContainer>
+      <MainBtnContainer>
+        <MainBtn onClick={onClick}>
+          <BtnTopContainer>
+            <BtnTitle>PART</BtnTitle>
+            <Arrow_IC />
+          </BtnTopContainer>
+          <BtnCaption>파트별 소개와 세션 개요 알아보기</BtnCaption>
+        </MainBtn>
+        <MainBtn>
+          <BtnTopContainer>
+            <BtnTitle>PART</BtnTitle>
+            <Arrow_IC />
+          </BtnTopContainer>
+          <BtnCaption>파트별 소개와 세션 개요 알아보기</BtnCaption>
+        </MainBtn>
+        <MainBtn>
+          <BtnTopContainer>
+            <BtnTitle>PART</BtnTitle>
+            <Arrow_IC />
+          </BtnTopContainer>
+          <BtnCaption>파트별 소개와 세션 개요 알아보기</BtnCaption>
+        </MainBtn>
+      </MainBtnContainer>
+    </>
+  );
 };
 
 export default Home;
+
+const BgImgWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-image: url(${bgImg});
+  background-size: cover;
+  background-position: center;
+  z-index: -1;
+`;
+
+const BgWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: -1;
+`;
+
+const MainContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 8rem;
+  margin: 0 8.5rem;
+  margin-top: 50.3rem;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+`;
+
+const Title = styled.div`
+  /* font-family: "SUIT Variable"; */
+  font-size: 6rem;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  background: linear-gradient(90deg, #1cffff 0%, #74c8ff 48.5%, #bab0ff 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const SubTitle = styled.div`
+  color: white;
+  /* font-family: Pretendard; */
+  font-size: 3rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const MainBtnContainer = styled.div`
+  display: flex;
+  gap: 4rem;
+  width: 100%;
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.8) 100%);
+  padding: 5.1rem 8.5rem 8rem 8.5rem;
+`;
+
+const MainBtn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.9rem;
+  width: 45rem;
+  height: 12rem;
+  padding: 2rem 2.2rem;
+  flex-direction: column;
+  justify-content: center;
+  background: rgba(255, 255, 255, 0.1);
+  &:hover {
+    background: linear-gradient(90deg, rgba(94, 94, 94, 0.22) 34.44%, rgba(0, 52, 148, 0.7) 100%);
+  }
+`;
+
+const BtnTopContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const BtnTitle = styled.div`
+  color: white;
+  /* font-family: "SUIT Variable"; */
+  font-size: 3rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 0.6px;
+`;
+
+const BtnCaption = styled.div`
+  color: var(--Basic-White, #fff);
+  /* font-family: Pretendard; */
+  font-size: 1.8rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.18px;
+`;
