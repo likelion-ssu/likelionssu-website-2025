b969a81eee0d037b7d2fd8a319dcddd9b2e71829
hyun907 <seunghyun020907@naver.com>
2025-02-05 22:21:18 +0900

    feat: 컴포넌트 분리

---
+++ src/Home/components/MainBtn.tsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Arrow_IC from "../assets/ic_arrow_up_right.svg?react";
+
+interface MainBtnProps {
+  title: string;
+  caption: string;
+  to: string;
+}
+
+const MainBtn = ({ title, caption, to }: MainBtnProps) => {
+  return (
+    <StyledMainBtn to={to}>
+      <BtnTopContainer>
+        <BtnTitle>{title}</BtnTitle>
+        <Arrow_IC />
+      </BtnTopContainer>
+      <BtnCaption>{caption}</BtnCaption>
+    </StyledMainBtn>
+  );
+};
+
+export default MainBtn;
+
+const StyledMainBtn = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.9rem;
+
+  width: 45rem;
+  height: 12rem;
+  padding: 2rem 2.2rem;
+
+  background: rgba(255, 255, 255, 0.1);
+  cursor: pointer;
+  text-decoration: none;
+
+  &:hover {
+    background: linear-gradient(90deg, rgba(94, 94, 94, 0.22) 34.44%, rgba(0, 52, 148, 0.7) 100%);
+  }
+`;
+
+const BtnTopContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+`;
+
+const BtnTitle = styled.div`
+  color: white;
+  font-size: 3rem;
+  font-weight: 700;
+  letter-spacing: 0.06rem;
+`;
+
+const BtnCaption = styled.div`
+  color: white;
+  font-size: 1.8rem;
+  font-weight: 400;
+  letter-spacing: -0.018rem;
+`;
--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,15 +1,9 @@
 import styled from "styled-components";
 import bgImg from "./assets/background.png";
-import Arrow_IC from "./assets/ic_arrow_up_right.svg?react";
-import { useNavigate } from "react-router-dom";
+import { mainBtns } from "./constants/mainBtns";
+import MainBtn from "./components/MainBtn";
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  const onClick = () => {
-    navigate(`/about`);
-  };
-
   return (
     <>
       <BgImgWrapper />
@@ -20,28 +14,11 @@ const Home = () => {
           <SubTitle>숭실대학교 멋쟁이사자처럼에서 실현하세요!</SubTitle>
         </TextContainer>
       </MainContainer>
+
       <MainBtnContainer>
-        <MainBtn onClick={onClick}>
-          <BtnTopContainer>
-            <BtnTitle>PART</BtnTitle>
-            <Arrow_IC />
-          </BtnTopContainer>
-          <BtnCaption>파트별 소개와 세션 개요 알아보기</BtnCaption>
-        </MainBtn>
-        <MainBtn>
-          <BtnTopContainer>
-            <BtnTitle>PART</BtnTitle>
-            <Arrow_IC />
-          </BtnTopContainer>
-          <BtnCaption>파트별 소개와 세션 개요 알아보기</BtnCaption>
-        </MainBtn>
-        <MainBtn>
-          <BtnTopContainer>
-            <BtnTitle>PART</BtnTitle>
-            <Arrow_IC />
-          </BtnTopContainer>
-          <BtnCaption>파트별 소개와 세션 개요 알아보기</BtnCaption>
-        </MainBtn>
+        {mainBtns.map((btn, index) => (
+          <MainBtn key={index} title={btn.title} caption={btn.caption} to={btn.to} />
+        ))}
       </MainBtnContainer>
     </>
   );
@@ -53,7 +30,6 @@ const BgImgWrapper = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-
   width: 100%;
   height: 100vh;
 
@@ -68,7 +44,6 @@ const BgWrapper = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-
   width: 100%;
   height: 100vh;
 
@@ -94,11 +69,8 @@ const TextContainer = styled.div`
 `;
 
 const Title = styled.div`
-  /* font-family: "SUIT Variable"; */
   font-size: 6rem;
-  font-style: normal;
   font-weight: 800;
-  line-height: normal;
 
   background: linear-gradient(90deg, #1cffff 0%, #74c8ff 48.5%, #bab0ff 100%);
   background-clip: text;
@@ -107,66 +79,16 @@ const Title = styled.div`
 `;
 
 const SubTitle = styled.div`
-  /* font-family: Pretendard; */
   color: white;
   font-size: 3rem;
-  font-style: normal;
   font-weight: 400;
-  line-height: normal;
 `;
 
 const MainBtnContainer = styled.div`
   display: flex;
   gap: 4rem;
-
   width: 100%;
+
   background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.8) 100%);
   padding: 5.1rem 8.5rem 8rem 8.5rem;
-`;
-
-const MainBtn = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.9rem;
-  flex-direction: column;
-  justify-content: center;
-
-  width: 45rem;
-  height: 12rem;
-  padding: 2rem 2.2rem;
-
-  background: rgba(255, 255, 255, 0.1);
-
-  cursor: pointer;
-  &:hover {
-    background: linear-gradient(90deg, rgba(94, 94, 94, 0.22) 34.44%, rgba(0, 52, 148, 0.7) 100%);
-  }
-`;
-
-const BtnTopContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  width: 100%;
-`;
-
-const BtnTitle = styled.div`
-  /* font-family: "SUIT Variable"; */
-  color: white;
-  font-size: 3rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  letter-spacing: 0.06rem;
-`;
-
-const BtnCaption = styled.div`
-  /* font-family: Pretendard; */
-  color: white;
-  font-size: 1.8rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  letter-spacing: -0.018rem;
 `;
