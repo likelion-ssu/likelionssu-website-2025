ac745ad51a987cf0272e1e476f3b1901a99d2604
yminjuu <ymj0580@naver.com>
2025-02-10 23:18:51 +0900

    fix: 사진 변경사항 반영

--- src/About/constants/activityArrays.ts
+++ src/About/constants/activityArrays.ts
@@ -6,21 +6,25 @@ export const imgPaths = [
   "images/activities/img_hcktn1.png",
   "images/activities/img_hcktn2.png",
   "images/activities/img_hcktn3.png",
+  "images/activities/img_hcktn4.png",
   "images/activities/img_ughHeung1.png",
   "images/activities/img_ughHeung2.png",
+  "images/activities/img_ughHeung3.png",
   "images/activities/img_network1.png",
   "images/activities/img_network2.png",
   "images/activities/img_winterProj1.png",
   "images/activities/img_winterProj2.png",
+  "images/activities/img_winterProj3.png",
   "images/activities/img_social1.png",
-  "images/activities/img_social2.png"
+  "images/activities/img_social2.png",
+  "images/activities/img_social3.png"
 ];
 
 export const imgToIndexMap = [
   { min: 0, max: 1, index: ActivityType.EDUCATION },
-  { min: 2, max: 4, index: ActivityType.HACKATHON },
-  { min: 5, max: 6, index: ActivityType.UGHHEUNGCON },
-  { min: 7, max: 8, index: ActivityType.NETWORKING },
-  { min: 9, max: 10, index: ActivityType.WINTERPROJECT },
-  { min: 11, max: 12, index: ActivityType.SOCIAL }
+  { min: 2, max: 5, index: ActivityType.HACKATHON },
+  { min: 6, max: 8, index: ActivityType.UGHHEUNGCON },
+  { min: 9, max: 10, index: ActivityType.NETWORKING },
+  { min: 11, max: 13, index: ActivityType.WINTERPROJECT },
+  { min: 14, max: 16, index: ActivityType.SOCIAL }
 ];
