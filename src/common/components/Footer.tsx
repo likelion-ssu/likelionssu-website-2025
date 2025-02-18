777c13dfaa3dfd4d0917776df9a5f6da2b3b66a7
xionng <leeso0717@naver.com>
2025-02-19 01:45:57 +0900

    style: 푸터 모바일 반응형 적용

--- src/common/components/Footer.tsx
+++ src/common/components/Footer.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import media from "../../common/styles/media";
 
 const Footer = () => {
   return (
@@ -9,7 +10,7 @@ const Footer = () => {
           <li>TECHIT</li>
         </ContentsContainer>
         <RightContentsContainer>
-          CONTACT
+          <ContactMent>CONTACT</ContactMent>
           <ContactContainer>
             <li>ssu@likelion.org</li>
             <a href="https://www.instagram.com/likelion_ssu/">
@@ -35,21 +36,32 @@ const FooterContainer = styled.div`
   justify-content: space-between;
 
   background-color: black;
+
+  ${media.small`
+    flex-direction: column;
+    align-items :  start;
+    gap:1.8rem;
+    padding: 2rem 2rem;
+    justify-content: center;
+  `};
 `;
 
+const ContactMent = styled.p`
+  ${media.small`
+    display:none;
+  `};
+`;
 const ContentsContainer = styled.div`
   display: flex;
   flex-direction: column;
   color: var(--30, #9a9a9a);
   font-feature-settings: "calt" off;
-
-  font-family: "SUIT Variable";
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  letter-spacing: 0.32px;
   gap: 1.2rem;
+  ${({ theme }) => theme.mixins.font(theme.fonts.Suit.body3)}
+
+  ${({ theme }) => media.small`
+    ${theme.mixins.font(theme.fonts.Suit.body5)}
+  `};
 `;
 
 const RightContentsContainer = styled(ContentsContainer)`
