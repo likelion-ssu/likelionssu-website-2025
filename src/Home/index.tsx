cbe24b5c6997de1f8e0867096c1162114bcf90ff
hyun907 <seunghyun020907@naver.com>
2025-02-21 01:23:22 +0900

    feat: 홈 모바일 반응형 대응 및 yarn 버전 이전 파일 복구

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -3,6 +3,13 @@ import { mainBtns } from "./constants/mainBtns";
 import MainBtn from "./components/MainBtn";
 import TopBar from "../common/components/TopBar";
 import Footer from "../common/components/Footer";
+import media from "../common/styles/media";
+import type { DefaultTheme } from "styled-components";
+
+import pc_titleImg from "./assets/pc_possible.png";
+import possibleImg from "./assets/possible.png";
+import toImg from "./assets/to.png";
+import realityImg from "./assets/reality.png";
 
 const Home = () => {
   return (
@@ -12,17 +19,28 @@ const Home = () => {
         <MainContainer>
           <TextContainer>
             <TitleWrapper>
-              <Title>POSSIBILITY TO REALITY</Title>
+              <Title>POSSIBILITY&nbsp;TO&nbsp;&nbsp;REALITY</Title>
+              <img src={pc_titleImg} />
             </TitleWrapper>
 
-            <TitleBgWrapperTop>
-              <TitleBg data-content="POSSIBILITY TO REALITY">POSSIBILITY TO REALITY</TitleBg>
-            </TitleBgWrapperTop>
+            <MobileTitleWrapper>
+              <MobileTitleBg>
+                <MobileTitleItem>POSSIBILITY</MobileTitleItem>
+                <img src={possibleImg} />
+              </MobileTitleBg>
 
-            <TitleBgWrapperBottom>
-              <TitleBg data-content="POSSIBILITY TO REALITY">POSSIBILITY TO REALITY</TitleBg>
-            </TitleBgWrapperBottom>
-            <SubTitle>숭실대학교 멋쟁이사자처럼에서 실현하세요!</SubTitle>
+              <MobileTitleBg>
+                <MobileTitleItem>TO</MobileTitleItem>
+                <img src={toImg} />
+              </MobileTitleBg>
+
+              <MobileTitleBg>
+                <MobileTitleItem>REALITY</MobileTitleItem>
+                <img src={realityImg} />
+              </MobileTitleBg>
+            </MobileTitleWrapper>
+
+            <SubTitle>숭실대학교 멋쟁이사자처럼에서 실현하세요</SubTitle>
           </TextContainer>
         </MainContainer>
 
@@ -42,10 +60,17 @@ export default Home;
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   gap: 8rem;
 
   margin: 0 8.5rem;
   margin-top: 50.3rem;
+
+  ${media.small`
+    margin: 0 5.5rem;
+    margin-top: 18.2rem;
+  `}
 `;
 
 const TextContainer = styled.div`
@@ -55,6 +80,63 @@ const TextContainer = styled.div`
   align-items: center;
   gap: 4rem;
   position: relative;
+  min-width: 102.7rem;
+
+  ${media.medium`
+    min-width: 100%;
+  `};
+
+  ${media.small`
+    min-width: 100%;
+    gap: 5rem;
+  `};
+`;
+
+const MobileTitleWrapper = styled.div`
+  display: none;
+
+  ${media.small`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    gap: 3.443rem;
+  `};
+`;
+
+const MobileTitleBg = styled.div`
+  display: none;
+
+  ${media.small`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 5.6rem;
+    position: relative;
+
+    background-color: ${({ theme }: DefaultTheme) => theme.colors.Black};
+
+  `};
+`;
+
+const MobileTitleItem = styled.div`
+  display: none;
+
+  ${media.small`
+    position: absolute;
+    left: 0;
+
+    display: flex;
+    background: linear-gradient(90deg, #1cffff 0%, #74c8ff 48.5%, #bab0ff 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+    font-family: "SUIT Variable";
+    font-size: 4.4506rem;
+    font-weight: 900;
+`};
 `;
 
 const TitleWrapper = styled.div`
@@ -62,52 +144,27 @@ const TitleWrapper = styled.div`
   justify-content: center;
   align-items: center;
   height: 10rem;
+  position: relative;
 
   background-color: ${({ theme }) => theme.colors.Black};
+
+  ${media.small`
+    display: none;
+  `};
 `;
 
 const Title = styled.div`
-  font-family: "SUIT Variable";
-  font-size: 8rem;
-  font-weight: 800;
+  position: absolute;
+  left: 0;
 
   background: linear-gradient(90deg, #1cffff 0%, #74c8ff 48.5%, #bab0ff 100%);
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-`;
 
-const TitleBgWrapperTop = styled.div`
-  position: absolute;
-  z-index: -1;
-  top: -4rem;
-`;
-
-const TitleBgWrapperBottom = styled.div`
-  position: absolute;
-  z-index: -1;
-  top: 6rem;
-`;
-
-const TitleBg = styled.div`
   font-family: "SUIT Variable";
   font-size: 8rem;
-  font-style: normal;
   font-weight: 900;
-  letter-spacing: -0.08rem;
-  color: transparent;
-  z-index: 0;
-  position: relative;
-  color: ${({ theme }) => theme.colors.Black};
-
-  &::before {
-    z-index: -1;
-    content: attr(data-content);
-    position: absolute;
-    left: 0;
-    -webkit-text-stroke-width: 2px;
-    -webkit-text-stroke-color: rgba(255, 255, 255, 0.3);
-  }
 `;
 
 const SubTitle = styled.div`
@@ -118,6 +175,18 @@ const SubTitle = styled.div`
   font-style: normal;
   font-weight: 400;
   line-height: normal;
+
+  ${media.small`
+    color: #C6C6C6;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 2.6rem;
+    letter-spacing: -0.016rem;
+    width: 28rem;
+  `}
 `;
 
 const MainBtnContainer = styled.div`
@@ -127,6 +196,15 @@ const MainBtnContainer = styled.div`
 
   background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.8) 100%);
   padding: 5.1rem 8.5rem 8rem 8.5rem;
+
+  ${media.small`
+    flex-direction: column;
+    padding: 2rem 3rem;
+    gap: 1.2rem;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  `}
 `;
 
 const Container = styled.div`
