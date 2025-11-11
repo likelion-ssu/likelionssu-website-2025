da74ec4dc135bfe2b8a8203a6509b10bf47270e9
yminjuu <ymj0580@naver.com>
2025-11-12 01:07:06 +0900

    feat: activity 사진, 설명, 순서 변경

--- src/About/constants/activityArrays.ts
+++ src/About/constants/activityArrays.ts
@@ -1,34 +1,39 @@
 import { ActivityType } from "../type/activity";
 
 export const imgPaths = [
-  "images/activities/img_edu1.webp",
-  "images/activities/img_edu2.webp",
+  // (1) 교육
+  "images/activities/img_edu1.png",
+  "images/activities/img_edu2.png",
+  "images/activities/img_edu3.png",
+  "images/activities/img_edu4.webp",
+  // (2) 멘토링
   "images/activities/img_mentoring1.webp",
-  "images/activities/img_mentoring2.webp",
+  "images/activities/img_mentoring2.png",
   "images/activities/img_mentoring3.webp",
-  // 멘토링 사진 3가지 추가
-  "images/activities/img_hcktn1.webp",
-  "images/activities/img_hcktn2.webp",
-  "images/activities/img_hcktn3.webp",
-  "images/activities/img_hcktn4.webp",
-  "images/activities/img_ughHeung1.webp",
-  "images/activities/img_ughHeung2.webp",
-  "images/activities/img_ughHeung3.webp",
-  "images/activities/img_network1.webp",
-  "images/activities/img_network2.webp",
-  // "images/activities/img_winterProj1.webp",
-  // "images/activities/img_winterProj2.webp",
-  // "images/activities/img_winterProj3.webp",
+  // (3) 해커톤
+  "images/activities/img_hcktn1.png",
+  "images/activities/img_hcktn2.png",
+  "images/activities/img_hcktn3.png",
+  "images/activities/img_hcktn4.png",
+  "images/activities/img_hcktn5.webp",
+  // (4) 전국 단위 행사
+  "images/activities/img_ughHeung1.png",
+  "images/activities/img_ughHeung2.png",
+  // (5) 네트워킹 & 친목
   "images/activities/img_social1.webp",
   "images/activities/img_social2.webp",
-  "images/activities/img_social3.webp"
+  "images/activities/img_social3.webp",
+  // (6) 실리콘 밸리
+  "images/activities/img_silicon1.webp",
+  "images/activities/img_silicon2.png",
+  "images/activities/img_silicon3.png",
 ];
 
 export const imgToIndexMap = [
-  { min: 0, max: 1, index: ActivityType.EDUCATION },
-  { min: 2, max: 4, index: ActivityType.MENTORING },
-  { min: 5, max: 8, index: ActivityType.HACKATHON },
-  { min: 9, max: 11, index: ActivityType.UGHHEUNGCON },
-  { min: 12, max: 13, index: ActivityType.NETWORKING },
-  { min: 14, max: 16, index: ActivityType.SOCIAL }
+  { min: 0, max: 3, index: ActivityType.EDUCATION },
+  { min: 4, max: 6, index: ActivityType.MENTORING },
+  { min: 7, max: 11, index: ActivityType.HACKATHON },
+  { min: 12, max: 13, index: ActivityType.UGHHEUNGCON },
+  { min: 14, max: 16, index: ActivityType.SOCIAL },
+  { min: 17, max: 19, index: ActivityType.SILICON },
 ];
