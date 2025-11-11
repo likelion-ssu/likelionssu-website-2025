c029392ef4700a8af3222c4fb6a796ff6dfaefc9
yminjuu <ymj0580@naver.com>
2025-11-11 21:02:39 +0900

    feat: 활동 사진 및 텍스트 수정

--- src/About/constants/activityArrays.ts
+++ src/About/constants/activityArrays.ts
@@ -3,6 +3,10 @@ import { ActivityType } from "../type/activity";
 export const imgPaths = [
   "images/activities/img_edu1.webp",
   "images/activities/img_edu2.webp",
+  "images/activities/img_mentoring1.webp",
+  "images/activities/img_mentoring2.webp",
+  "images/activities/img_mentoring3.webp",
+  // 멘토링 사진 3가지 추가
   "images/activities/img_hcktn1.webp",
   "images/activities/img_hcktn2.webp",
   "images/activities/img_hcktn3.webp",
@@ -12,9 +16,9 @@ export const imgPaths = [
   "images/activities/img_ughHeung3.webp",
   "images/activities/img_network1.webp",
   "images/activities/img_network2.webp",
-  "images/activities/img_winterProj1.webp",
-  "images/activities/img_winterProj2.webp",
-  "images/activities/img_winterProj3.webp",
+  // "images/activities/img_winterProj1.webp",
+  // "images/activities/img_winterProj2.webp",
+  // "images/activities/img_winterProj3.webp",
   "images/activities/img_social1.webp",
   "images/activities/img_social2.webp",
   "images/activities/img_social3.webp"
@@ -22,9 +26,9 @@ export const imgPaths = [
 
 export const imgToIndexMap = [
   { min: 0, max: 1, index: ActivityType.EDUCATION },
-  { min: 2, max: 5, index: ActivityType.HACKATHON },
-  { min: 6, max: 8, index: ActivityType.UGHHEUNGCON },
-  { min: 9, max: 10, index: ActivityType.NETWORKING },
-  { min: 11, max: 13, index: ActivityType.WINTERPROJECT },
+  { min: 2, max: 4, index: ActivityType.MENTORING },
+  { min: 5, max: 8, index: ActivityType.HACKATHON },
+  { min: 9, max: 11, index: ActivityType.UGHHEUNGCON },
+  { min: 12, max: 13, index: ActivityType.NETWORKING },
   { min: 14, max: 16, index: ActivityType.SOCIAL }
 ];
--- src/About/constants/activityList.ts
+++ src/About/constants/activityList.ts
@@ -14,30 +14,36 @@ export const ACTIVITY_LIST: Record<
     description:
       "프로젝트에 필요한 핵심 스킬을 체계적으로 익혀요.\n파트장의 생생한 경험이 담긴 강연과 파트원들과의 활발한 소통을 통해 함께 성장하는 시간이에요."
   },
-  [ActivityType.HACKATHON]: {
+  [ActivityType.MENTORING]: {
     order: 2,
-    title: "02 중앙 해커톤",
+    title: "02 멘토링",
     description:
-      "무박 2일 동안 모든 멋사가 모여 개발하는 연례 행사예요.\n짧은 기간동안 열정을 불태워 팀원들과 함께 아이디어를 현실로 구현하는 짜릿한 경험이에요."
+      "1년 간의 활동을 진행함에 있어 운영진들의 멘토링은 물론\n사회 각 분야에서 활동 중이신 선배 사자님들의 멘토링을 통해 실무적인 조언과 피드백을 받을 수 있어요."
+  },
+  [ActivityType.HACKATHON]: {
+    order: 3,
+    title: "03 해커톤",
+    description:
+      "전국 해커톤에 참여해요. 그 중 중앙 해커톤은 무박 2일 동안 모든 멋사가 모여 개발하는 연례 행사예요.\n이러한 대회 참여는 짧은 기간동안 열정을 불태워 팀원들과 함께 아이디어를 현실로 구현하는 짜릿한 경험이에요."
   },
   [ActivityType.UGHHEUNGCON]: {
-    order: 3,
-    title: "03 어흥콘",
+    order: 4,
+    title: "04 어흥콘",
     description:
       "각 대학에서 진행한 프로젝트를 출품하고 행사장에서 시연과 공유를 통해 아이디어를 나누는 행사예요.\n각 학교 학생들과 자유롭게 교류하며 다양한 아이디어를 얻을 수 있었어요."
   },
   [ActivityType.NETWORKING]: {
-    order: 4,
-    title: "04 네트워킹",
+    order: 5,
+    title: "05 네트워킹",
     description:
       "다양한 사람들과 아이디어를 공유하고 서로에게 배우며 함께 성장하는 기회예요. 선배 사자들의 조언으로 동기를 얻을 수 있어요."
   },
-  [ActivityType.WINTERPROJECT]: {
-    order: 5,
-    title: "05 겨울잠 프로젝트",
-    description:
-      "겨울잠은 하반기에 장기적으로 진행되는 프로젝트예요.\n기획부터 개발, 배포까지 전 과정을 경험하며 깊이 있는 서비스 개발 역량을 쌓을 수 있어요."
-  },
+  // [ActivityType.WINTERPROJECT]: {
+  //   order: 6,
+  //   title: "06 겨울잠 프로젝트",
+  //   description:
+  //     "겨울잠은 하반기에 장기적으로 진행되는 프로젝트예요.\n기획부터 개발, 배포까지 전 과정을 경험하며 깊이 있는 서비스 개발 역량을 쌓을 수 있어요."
+  // },
   [ActivityType.SOCIAL]: {
     order: 6,
     title: "06 친목 모임",
